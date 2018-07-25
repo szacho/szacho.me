0fe127ddd9fee0087b42ee32efe439c74a18069d
szacho <whywayaway@gmail.com>
2018-07-25 04:16:02 +0200

    Add styles for articles

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ TODO:
 - import medium post styling
 - enable code syntax highlighter for medium posts
 - fix entries list styling
-- 
+- fix categories links
--- _plugins/medium_feed.rb
+++ _plugins/medium_feed.rb
@@ -5,28 +5,48 @@ module Jekyll
     safe true
     priority :high
 
-def generate(site)
-  jekyll_coll = Jekyll::Collection.new(site, 'medium_feed')
-  site.collections['medium_feed'] = jekyll_coll
+    def generate(site)
+      jekyll_coll = Jekyll::Collection.new(site, 'medium_feed')
+      site.collections['medium_feed'] = jekyll_coll
 
-feed_url = "https://towardsdatascience.com/feed"
-# feed_url = "https://medium.com/feed/@szacho"
-Feedjira::Feed.fetch_and_parse(feed_url).entries.each do |e|
-        # p "Title: #{e.title}, published on Medium #{e.url} #{e}"
+    def formatContent(content)
+      content.gsub! '<h3>', '<h2>'
+      content.gsub! '</h3>', '</h2>'
+      content.gsub! '<h4>', '<h3>'
+      content.gsub! '</h4>', '</h3>'
+      return content
+    end
+
+    feed_url = "https://blog.usejournal.com/feed"
+    # feed_url = "https://medium.com/feed/@szacho"
+    Feedjira::Feed.fetch_and_parse(feed_url).entries.each do |e|
+        i = 0
+        categories = []
+        while i <= 2 # limit categories, dev only
+          if e[:categories][i]
+            categories.push(e[:categories][i].split('-').join(' '))
+          end
+          i += 1
+        end
         title = e[:title]
         content = e[:content]
         date = e[:published]
-        categories = e[:categories]
         guid = e[:url]
         path = "./_medium_feed/" + title.downcase + ".md"
         path = site.in_source_dir(path)
         doc = Jekyll::Document.new(path, { :site => site, :collection => jekyll_coll })
-        doc.data['title'] = title;
-        doc.data['feed_content'] = content;
+
+
+        doc.data['feed_content'] = formatContent(content)
+
+
+        doc.data['title'] = title
         doc.data['date'] = date
+        doc.data['origin'] = guid
         doc.data['categories'] = categories
         doc.data['layout'] = 'article'
         doc.data['isMedium'] = true
+        doc.data['comments'] = true
         jekyll_coll.docs << doc
       end
     end
